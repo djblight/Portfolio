ef38f4931187d2eb99f86d8a4de8ba8dbbf1087b
djblight <djblight@bellsouth.net>
2017-09-29 18:57:26 -0400

    commit

--- javascript.js
+++ javascript.js
@@ -1,4 +1,22 @@
 $(function() {
+    var pull 		= $('#pull');
+        menu 		= $('nav ul');
+        menuHeight	= menu.height();
+
+    $(pull).on('click', function(e) {
+        e.preventDefault();
+        menu.slideToggle();
+    });
+
+    $(window).resize(function(){
+        var w = $(window).width();
+        if(w > 320 && menu.is(':hidden')) {
+            menu.removeAttr('style');
+        }
+    });
+});
+
+$(function() {
     
             $(".knob").knob({
                 draw : function () {
@@ -92,3 +110,31 @@ $(function() {
               topLayer.style.width= e.clientX + skew + delta + 'px';
             });
           });
+
+    // Modal
+    // Get the modal
+var modal = document.getElementById('myModal');
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal 
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
