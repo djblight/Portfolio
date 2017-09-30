c36c2fdd037c6010e0199720030d7d6138d9e3c3
djblight <djblight@bellsouth.net>
2017-09-30 10:07:56 -0400

    commit

--- javascript.js
+++ javascript.js
@@ -1,118 +1,123 @@
 $(function() {
-    var pull 		= $('#pull');
-        menu 		= $('nav ul');
-        menuHeight	= menu.height();
+    var pull = $('#pull');
+    menu = $('nav ul');
+    menuHeight = menu.height();
 
     $(pull).on('click', function(e) {
         e.preventDefault();
         menu.slideToggle();
     });
 
-    $(window).resize(function(){
+    $(window).resize(function() {
         var w = $(window).width();
-        if(w > 320 && menu.is(':hidden')) {
+        if (w > 320 && menu.is(':hidden')) {
             menu.removeAttr('style');
         }
     });
 });
 
 $(function() {
-    
-            $(".knob").knob({
-                draw : function () {
-    
-                    // "tron" case
-                    if(this.$.data('skin') == 'tron') {
-    
-                        var a = this.angle(this.cv)  // Angle
-                                , sa = this.startAngle          // Previous start angle
-                                , sat = this.startAngle         // Start angle
-                                , ea                            // Previous end angle
-                                , eat = sat + a                 // End angle
-                                , r = true;
-    
-                        this.g.lineWidth = this.lineWidth;
-    
-                        this.o.cursor
-                        && (sat = eat - 0.3)
-                        && (eat = eat + 0.3);
-    
-                        if (this.o.displayPrevious) {
-                            ea = this.startAngle + this.angle(this.value);
-                            this.o.cursor
-                            && (sa = ea - 0.3)
-                            && (ea = ea + 0.3);
-                            this.g.beginPath();
-                            this.g.strokeStyle = this.previousColor;
-                            this.g.arc(this.xy, this.xy, this.radius - this.lineWidth, sa, ea, false);
-                            this.g.stroke();
-                        }
-    
-                        this.g.beginPath();
-                        this.g.strokeStyle = r ? this.o.fgColor : this.fgColor ;
-                        this.g.arc(this.xy, this.xy, this.radius - this.lineWidth, sat, eat, false);
-                        this.g.stroke();
-    
-                        this.g.lineWidth = 2;
-                        this.g.beginPath();
-                        this.g.strokeStyle = this.o.fgColor;
-                        this.g.arc(this.xy, this.xy, this.radius - this.lineWidth + 1 + this.lineWidth * 2 / 3, 0, 2 * Math.PI, false);
-                        this.g.stroke();
-    
-                        return false;
-                    }
+
+    $(".knob").knob({
+        draw: function() {
+
+            // "tron" case
+            if (this.$.data('skin') == 'tron') {
+
+                var a = this.angle(this.cv) // Angle
+                    ,
+                    sa = this.startAngle // Previous start angle
+                    ,
+                    sat = this.startAngle // Start angle
+                    ,
+                    ea // Previous end angle
+                    , eat = sat + a // End angle
+                    ,
+                    r = true;
+
+                this.g.lineWidth = this.lineWidth;
+
+                this.o.cursor &&
+                    (sat = eat - 0.3) &&
+                    (eat = eat + 0.3);
+
+                if (this.o.displayPrevious) {
+                    ea = this.startAngle + this.angle(this.value);
+                    this.o.cursor &&
+                        (sa = ea - 0.3) &&
+                        (ea = ea + 0.3);
+                    this.g.beginPath();
+                    this.g.strokeStyle = this.previousColor;
+                    this.g.arc(this.xy, this.xy, this.radius - this.lineWidth, sa, ea, false);
+                    this.g.stroke();
                 }
-            });
-        });
-    
-        function tp (s) {
-            s += "";
-            if (s.length === 1)
-                s = "0" + s;
-            return s;
-        }
-        function clock() {
-            var $s = $(".second");
-            var $m = $(".minute");
-            var $h = $(".hour");
-            var d = new Date();
-            var s = d.getSeconds();
-            var ms = d.getMilliseconds();
-            var m = d.getMinutes();
-            var h = d.getHours();
-            $s.val((s*1000)+ms).trigger("change");
-            $m.val((m * 60)+s).trigger("change");
-            $h.val((h*60)+m).trigger("change");
-    
-            $('span.hour').html(tp(h));
-            $('span.minute').html(tp(m));
-            $('span.second').html(tp(s));
-            setTimeout("clock()", 30);
-        }
-        clock();
 
-        document.addEventListener('DOMContentLoaded', function(){
-            let wrapper = document.getElementById('wrapper');
-            let topLayer = wrapper.querySelector('.top');
-            let handle = wrapper.querySelector('.handle');
-            let skew = 0;
-            let delta = 0;
-          
-            if(wrapper.className.indexOf('skewed') != -1){
-              skew = 1000;
+                this.g.beginPath();
+                this.g.strokeStyle = r ? this.o.fgColor : this.fgColor;
+                this.g.arc(this.xy, this.xy, this.radius - this.lineWidth, sat, eat, false);
+                this.g.stroke();
+
+                this.g.lineWidth = 2;
+                this.g.beginPath();
+                this.g.strokeStyle = this.o.fgColor;
+                this.g.arc(this.xy, this.xy, this.radius - this.lineWidth + 1 + this.lineWidth * 2 / 3, 0, 2 * Math.PI, false);
+                this.g.stroke();
+
+                return false;
             }
-            
-            wrapper.addEventListener('mousemove', function(e){
-              delta = (e.clientX - window.innerWidth / 2) * 0.5;
-            
-              handle.style.left = e.clientX + delta + 'px';
-          
-              topLayer.style.width= e.clientX + skew + delta + 'px';
-            });
-          });
+        }
+    });
+});
 
-    // Modal
-    // Get the modal
+function tp(s) {
+    s += "";
+    if (s.length === 1)
+        s = "0" + s;
+    return s;
+}
+
+function clock() {
+    var $s = $(".second");
+    var $m = $(".minute");
+    var $h = $(".hour");
+    var d = new Date();
+    var s = d.getSeconds();
+    var ms = d.getMilliseconds();
+    var m = d.getMinutes();
+    var h = d.getHours();
+    $s.val((s * 1000) + ms).trigger("change");
+    $m.val((m * 60) + s).trigger("change");
+    $h.val((h * 60) + m).trigger("change");
+
+    $('span.hour').html(tp(h));
+    $('span.minute').html(tp(m));
+    $('span.second').html(tp(s));
+    setTimeout("clock()", 30);
+}
+clock();
+
+document.addEventListener('DOMContentLoaded', function() {
+    let wrapper = document.getElementById('wrapper');
+    let topLayer = wrapper.querySelector('.top');
+    let handle = wrapper.querySelector('.handle');
+    let skew = 0;
+    let delta = 0;
+
+    if (wrapper.className.indexOf('skewed') != -1) {
+        skew = 1000;
+    }
+
+    wrapper.addEventListener('mousemove', function(e) {
+        delta = (e.clientX - window.innerWidth / 2) * 0.5;
+
+        handle.style.left = e.clientX + delta + 'px';
+
+        topLayer.style.width = e.clientX + skew + delta + 'px';
+    });
+});
+
+// Modal
+// Get the modal
 var modal = document.getElementById('myModal');
 
 // Get the button that opens the modal
@@ -137,4 +142,3 @@ window.onclick = function(event) {
         modal.style.display = "none";
     }
 }
-
